073be0a183955e68383edbd109c3f07a6d1cf8a4
Mehmet Dağ <mdag.92988@protonmail.com>
2026-03-02 21:06:46 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-// ownership of this project belongs to: 6420f0400eb5a52e53f61b6b24bacc87124874070603c3d354c4772ccf3d7305
+// ownership of this project belongs to: github.com/reekta92 (Mehmet Dag)
 
 using System;
 using System.Threading;
@@ -50,4 +50,5 @@ namespace screenCap
         }
     }
 }
+
 
